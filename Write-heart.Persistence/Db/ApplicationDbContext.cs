752b666fbeaa79bcf93e6740c188aa003ddd7941
Ankit Podal <ankisharma864@gmail.com>
2025-02-08 21:50:13 +0530

    db context

---
+++ Write-heart.Persistence/Db/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Write_Heart.Domain.Entities;
+
+namespace Write_heart.Persistence.Db
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; } // Creates Users table
+    }
+}
